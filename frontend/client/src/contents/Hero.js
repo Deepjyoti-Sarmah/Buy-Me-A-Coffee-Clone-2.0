c6b8d6375c8b7a6eabdd63f2e4b1f5c45ea28dc6
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-01-28 15:17:54 +0530

    front end work done

---
+++ frontend/client/src/contents/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <>
+            <section className="max-w-lg lg:max-w-3xl mx-auto mt-12 md:mt-28 text-center">
+                <article>
+                    <h1 className="font-circular text-4xl sm:text-7xl text-slate-900 font-bold leading-normal md:leading-[1.2]">
+                        A supporter is worth a thousand followers.
+                    </h1>
+                    <p className="font-circular md:max-w-2xl md:mx-auto my-6 text-lg md:text-2xl text-gray-600 leading-normal">
+                        A Decentralized application of Buy me a Coffee
+                    </p>
+                </article>
+            </section>
+        </>
+    );
+};
+
+export default Hero;
